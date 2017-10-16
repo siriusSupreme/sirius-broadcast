01d789e6cdb7cdfb77a2407025bbdeaa922e5286
siriusSupreme <guoplc@163.com>
2017-10-16 17:42:08 +0800

    ''

---
+++ src/Contracts/ShouldBroadcast.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sirius\Broadcast\Contracts;
+
+interface ShouldBroadcast
+{
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn();
+}
---
+++ src/Contracts/ShouldBroadcastNow.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Sirius\Broadcast\Contracts;
+
+interface ShouldBroadcastNow extends ShouldBroadcast
+{
+    //
+}
